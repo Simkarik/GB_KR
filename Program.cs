fb5201acea3dd0a0d8fc4a7250bdc4681d827b1e
Artyom Semashko <simkaqwick@gmail.com>
2023-03-28 20:39:17 +0300

    started on code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Enter quantity of strings you want to type in: ");
+int num = int.Parse(Console.ReadLine());
+string[] array = new string[num];
+
+void GetMas(string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine($"Enter string {i+1}: ");
+        mas[i] = Console.ReadLine();
+    }
+}
+
+void PrintMass(string[] mass)
+{
+    int pos = 0;
+    while (pos< mass.Length -1)
+    {
+        Console.Write($"{mass[pos]}, ");
+        pos++;
+    }
+    Console.Write(mass[pos]);
+    Console.WriteLine();
+}
+
+
+
+GetMas(array);
+PrintMass(array);
